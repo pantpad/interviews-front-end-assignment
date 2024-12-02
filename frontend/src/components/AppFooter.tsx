4aa5674ecf69a0aff3ab68db77712743aec41621
pantpad <138708449+pantpad@users.noreply.github.com>
2024-12-02 15:23:23 +0100

    style(footer): add basic styling and text

--- frontend/src/components/AppFooter.tsx
+++ frontend/src/components/AppFooter.tsx
@@ -1,3 +1,34 @@
+import { Link } from '@tanstack/react-router'
+
 export default function AppFooter() {
-    return <footer>AppFooter</footer>
+    return (
+        <footer className="flex min-h-24 flex-wrap justify-between gap-2 border-t border-zinc-200 p-2 py-4">
+            <div>
+                <h1 className="text-xl font-bold">Recipe Book</h1>
+                <p>Enhance your cooking experience</p>
+                <p>© 2024 Recipe Book</p>
+            </div>
+            <article>
+                <ul>
+                    <li>
+                        <Link to="/about">
+                            <strong>Support</strong>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/about">Guidelines</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">Feedback Hub</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">Cooking Tips</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">Get in Touch</Link>
+                    </li>
+                </ul>
+            </article>
+        </footer>
+    )
 }
