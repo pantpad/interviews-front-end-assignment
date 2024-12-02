48f3230ad6bf283e5bc3e94af08872e617447a0f
pantpad <138708449+pantpad@users.noreply.github.com>
2024-12-02 14:53:26 +0100

    refactor: remove Layout component

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -2,18 +2,17 @@ import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import AppHeader from '../components/AppHeader'
 import AppFooter from '../components/AppFooter'
-import Layout from '../components/ui/Layout'
 
 export const Route = createRootRoute({
     component: () => (
         <>
-            <Layout className="flex min-h-screen flex-col bg-zinc-100">
+            <div className="flex min-h-screen flex-col bg-zinc-100">
                 <AppHeader />
-                <Layout className="flex-1">
+                <main className="flex-1">
                     <Outlet />
-                </Layout>
+                </main>
                 <AppFooter />
-            </Layout>
+            </div>
             <TanStackRouterDevtools />
         </>
     ),
