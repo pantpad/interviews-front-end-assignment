ee394b0b790a4c9391fcabf424e04c1dfeb63774
pantpad <138708449+pantpad@users.noreply.github.com>
2024-12-02 15:53:48 +0100

    feat: add bottom pagination buttons

--- frontend/src/components/Recipes/RecipesPagination.tsx
+++ frontend/src/components/Recipes/RecipesPagination.tsx
@@ -1,3 +1,18 @@
 export default function RecipesPagination() {
-    return <div>RecipesPagination</div>
+    return (
+        <nav className="flex items-center gap-1">
+            {[1, 2, 3, 4, '...', 25].map((page, index) => (
+                <button
+                    key={index}
+                    className={`flex h-8 min-w-[32px] items-center justify-center rounded-full px-3 ${
+                        page === 1
+                            ? 'bg-red-500 text-white'
+                            : 'hover:bg-gray-100'
+                    } `}
+                >
+                    {page}
+                </button>
+            ))}
+        </nav>
+    )
 }
