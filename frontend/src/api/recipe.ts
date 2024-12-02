a5415219f5aedf3cd4cb3425aa3a47d2e2a424d7
pantpad <138708449+pantpad@users.noreply.github.com>
2024-12-02 16:16:31 +0100

    feat: add api folder with getRecipes method

---
+++ frontend/src/api/recipe.ts
@@ -0,0 +1,17 @@
+const endpoint = 'http://localhost:8080/recipes'
+
+export type Recipe = {
+    id: string
+    name: string
+    ingredients: string[]
+    instructions: string
+    cuisineId: string
+    dietId: string
+    difficultyId: string
+    image: string
+}
+
+export const getRecipes = async (page: number): Promise<Recipe[]> => {
+    const response = await fetch(`${endpoint}?page=${page}`)
+    return response.json()
+}
