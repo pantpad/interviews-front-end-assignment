3ef793085c3ddd2fc8365874b3595c86a5b75fcc
pantpad <138708449+pantpad@users.noreply.github.com>
2024-12-02 16:13:07 +0100

    feat: add pagination UI interactivity

--- frontend/src/components/Recipes/RecipesPagination.tsx
+++ frontend/src/components/Recipes/RecipesPagination.tsx
@@ -1,18 +1,57 @@
+import { Link } from '@tanstack/react-router'
+import { useState } from 'react'
+
 export default function RecipesPagination() {
+    const [currentPage, setCurrentPage] = useState(1)
+    const totalPages: number = 25
+
     return (
-        <nav className="flex items-center justify-center gap-1">
-            {[1, 2, 3, 4, '...', 25].map((page, index) => (
+        <nav className="flex items-center justify-center gap-2">
+            <Link
+                to="/recipes"
+                search={(prev) => ({
+                    ...prev,
+                    page: Math.max(1, currentPage - 1),
+                })}
+                onClick={() => setCurrentPage(currentPage - 1)}
+            >
                 <button
-                    key={index}
-                    className={`flex h-8 min-w-[32px] items-center justify-center rounded-full px-3 ${
-                        page === 1
-                            ? 'bg-red-500 text-white'
+                    disabled={currentPage === 1}
+                    className={`flex h-8 items-center justify-center rounded-full px-3 ${
+                        currentPage === 1
+                            ? 'text-gray-300'
                             : 'hover:bg-gray-100'
                     } `}
                 >
-                    {page}
+                    ←
                 </button>
-            ))}
+            </Link>
+
+            <div
+                className={`flex h-8 min-w-[32px] items-center justify-center rounded-full bg-red-500 px-3 text-white`}
+            >
+                {currentPage}
+            </div>
+
+            <Link
+                to="/recipes"
+                search={(prev) => ({
+                    ...prev,
+                    page: Math.min(totalPages, currentPage + 1),
+                })}
+                onClick={() => setCurrentPage(currentPage + 1)}
+            >
+                <button
+                    disabled={currentPage === totalPages}
+                    className={`flex h-8 items-center justify-center rounded-full px-3 ${
+                        currentPage === totalPages
+                            ? 'text-gray-300'
+                            : 'hover:bg-gray-100'
+                    } `}
+                >
+                    →
+                </button>
+            </Link>
         </nav>
     )
 }
