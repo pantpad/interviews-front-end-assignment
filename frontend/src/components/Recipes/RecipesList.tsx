7067bf07e0388b4246998a3b91bfed7cbab1da0b
pantpad <138708449+pantpad@users.noreply.github.com>
2024-12-02 16:26:47 +0100

    feat: make recipes fetching work

--- frontend/src/components/Recipes/RecipesList.tsx
+++ frontend/src/components/Recipes/RecipesList.tsx
@@ -1,11 +1,19 @@
+import { useLoaderData } from '@tanstack/react-router'
+
 import RecipesHeader from './RecipesHeader'
 import RecipesPagination from './RecipesPagination'
 
 export default function RecipesList() {
+    const recipes = useLoaderData({ from: '/recipes/' })
+
     return (
         <section>
             <RecipesHeader />
-            <></>
+            <div className="flex flex-col gap-4">
+                {recipes.map((recipe) => {
+                    return <div key={recipe.id}>{recipe.name}</div>
+                })}
+            </div>
             <RecipesPagination />
         </section>
     )
