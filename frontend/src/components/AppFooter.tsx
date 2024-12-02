b9c481c573f64459d1f2c7aa1f1594f8fc8ed17e
pantpad <138708449+pantpad@users.noreply.github.com>
2024-12-02 15:29:22 +0100

    style: add bg + match styling with header

--- frontend/src/components/AppFooter.tsx
+++ frontend/src/components/AppFooter.tsx
@@ -2,33 +2,35 @@ import { Link } from '@tanstack/react-router'
 
 export default function AppFooter() {
     return (
-        <footer className="flex min-h-24 flex-wrap justify-between gap-2 border-t border-zinc-200 p-2 py-4">
-            <div>
-                <h1 className="text-xl font-bold">Recipe Book</h1>
-                <p>Enhance your cooking experience</p>
-                <p>© 2024 Recipe Book</p>
+        <footer className="flex min-h-24 gap-2 border-t border-zinc-200 bg-white p-2 py-4">
+            <div className="mx-auto flex w-full max-w-7xl flex-wrap justify-between">
+                <div>
+                    <h1 className="text-xl font-bold">Recipe Book</h1>
+                    <p>Enhance your cooking experience</p>
+                    <p>© 2024 Recipe Book</p>
+                </div>
+                <article>
+                    <ul>
+                        <li>
+                            <Link to="/about">
+                                <strong>Support</strong>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/about">Guidelines</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">Feedback Hub</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">Cooking Tips</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">Get in Touch</Link>
+                        </li>
+                    </ul>
+                </article>
             </div>
-            <article>
-                <ul>
-                    <li>
-                        <Link to="/about">
-                            <strong>Support</strong>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/about">Guidelines</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">Feedback Hub</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">Cooking Tips</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">Get in Touch</Link>
-                    </li>
-                </ul>
-            </article>
         </footer>
     )
 }
