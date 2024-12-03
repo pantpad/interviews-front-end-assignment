d1113b1f862ca4770bd6fad62928bda0271d3a80
pantpad <138708449+pantpad@users.noreply.github.com>
2024-12-03 16:22:31 +0100

    refactor: recipesList index

--- frontend/src/components/Recipes/RecipesList.tsx
+++ frontend/src/components/Recipes/RecipesList.tsx
@@ -1,14 +1,15 @@
 import { RecipeCard, RecipesPagination, RecipesHeader } from '.'
+import { Recipe } from '../../api/recipe'
 
 export default function RecipesList() {
-    const recipes = useLoaderData({ from: '/recipes/' })
+    const recipes: Recipe[] = []
 
     return (
         <section className="flex flex-col gap-4">
             <RecipesHeader />
             <div className="flex flex-col gap-4">
                 {recipes.map((recipe) => {
-                    return <RecipeCard key={recipe.id} recipe={recipe} />
+                    return <RecipeCard key={recipe.id} />
                 })}
             </div>
             <RecipesPagination />
