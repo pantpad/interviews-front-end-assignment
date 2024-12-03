e0e4f0cc0ceb79f2d228207e6499a233a6d69fba
pantpad <138708449+pantpad@users.noreply.github.com>
2024-12-03 18:13:58 +0100

    feat: improve useFetch hook by using an abortController for race conditions and multiple requests

--- frontend/src/hooks/useData.tsx
+++ frontend/src/hooks/useData.tsx
@@ -5,24 +5,36 @@ export function useData<T>(url: string) {
     const [error, setError] = useState<Error | null>(null)
     const [loading, setLoading] = useState(false)
 
-    const fetchData = async (url: string) => {
-        setLoading(true)
-        setError(null)
-        try {
-            const response = await fetch(url)
-            const json = await response.json()
-            setData(json)
-        } catch (err) {
-            setError(
-                err instanceof Error ? err : new Error('An error occurred')
-            )
-        } finally {
-            setLoading(false)
-        }
-    }
-
     useEffect(() => {
-        fetchData(url)
+        const abortController = new AbortController()
+
+        const fetchData = async () => {
+            setLoading(true)
+            setError(null)
+
+            try {
+                const response = await fetch(url, {
+                    signal: abortController.signal,
+                })
+                if (!response.ok) {
+                    throw new Error('Failed to fetch data')
+                }
+                const json = await response.json()
+                setData(json)
+            } catch (error) {
+                if (error instanceof Error && error.name !== 'AbortError') {
+                    setError(error)
+                }
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchData()
+
+        return () => {
+            abortController.abort()
+        }
     }, [url])
 
     return { data, error, loading }
