4ba074f7fab0a71462886a40311799898afa9c63
pantpad <138708449+pantpad@users.noreply.github.com>
2024-12-03 16:09:55 +0100

    refactor: refactor __root into app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import { Outlet } from 'react-router'
+import AppHeader from './components/AppHeader'
+import AppFooter from './components/AppFooter'
+
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline text-red-500">Hello World</h1>
-    </>
-  );
+    return (
+        <>
+            <div className="flex min-h-screen flex-col bg-zinc-100">
+                <AppHeader />
+                <main className="flex flex-1 flex-col">
+                    <Outlet />
+                </main>
+                <AppFooter />
+            </div>
+        </>
+    )
 }
 
-export default App;
+export default App
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,20 +1,20 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+// import { createRootRoute, Outlet } from '@tanstack/react-router'
+// import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
-import AppHeader from '../components/AppHeader'
-import AppFooter from '../components/AppFooter'
+// import AppHeader from '../components/AppHeader'
+// import AppFooter from '../components/AppFooter'
 
-export const Route = createRootRoute({
-    component: () => (
-        <>
-            <div className="flex min-h-screen flex-col bg-zinc-100">
-                <AppHeader />
-                <main className="flex flex-1 flex-col">
-                    <Outlet />
-                </main>
-                <AppFooter />
-            </div>
-            <TanStackRouterDevtools />
-        </>
-    ),
-})
+// export const Route = createRootRoute({
+//     component: () => (
+//         <>
+//             <div className="flex min-h-screen flex-col bg-zinc-100">
+//                 <AppHeader />
+//                 <main className="flex flex-1 flex-col">
+//                     <Outlet />
+//                 </main>
+//                 <AppFooter />
+//             </div>
+//             <TanStackRouterDevtools />
+//         </>
+//     ),
+// })
