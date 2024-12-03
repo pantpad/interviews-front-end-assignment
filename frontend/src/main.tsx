cf871243541027508d3e5f5d44905a11f6f1b2e1
pantpad <138708449+pantpad@users.noreply.github.com>
2024-12-03 16:08:30 +0100

    chore: add routes initial cfg to main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter, Routes, Route } from 'react-router'
 
 import './index.css'
 import App from './App'
@@ -12,7 +12,9 @@ if (!rootElement.innerHTML) {
     root.render(
         <StrictMode>
             <BrowserRouter>
-                <App />
+                <Routes>
+                    <Route path="/" element={<App />} />
+                </Routes>
             </BrowserRouter>
         </StrictMode>
     )
