6e5fbd533666438e3752ebf53cd7e2cf82072af2
pantpad <138708449+pantpad@users.noreply.github.com>
2024-12-03 15:59:15 +0100

    chore: add browser react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,24 +1,19 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+
+import { BrowserRouter } from 'react-router'
+
 import './index.css'
-
-const router = createRouter({ routeTree })
-
-// Register the router instance for type safety
-declare module '@tanstack/react-router' {
-    interface Register {
-        router: typeof router
-    }
-}
+import App from './App'
 
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
     const root = ReactDOM.createRoot(rootElement)
     root.render(
         <StrictMode>
-            <RouterProvider router={router} />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </StrictMode>
     )
 }
