d881fb5f2ac6b7ab4f4c7cd3d00ad36cb56a2dfe
pantpad <138708449+pantpad@users.noreply.github.com>
2024-12-05 15:01:16 +0100

    feat: add search params set on difficulty button click

--- frontend/src/components/Recipes/Sidebar/FiltersSection/RecipeDifficulty.tsx
+++ frontend/src/components/Recipes/Sidebar/FiltersSection/RecipeDifficulty.tsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router'
+
 export default function RecipeDifficulty() {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const difficultyId = searchParams.get('difficultyId') || ''
+    const [difficulty, setDifficulty] = useState(difficultyId)
+
+    const handleDifficultyChange = (difficulty: string) => {
+        setDifficulty(difficulty)
+    }
+
+    useEffect(() => {
+        if (difficulty === '') {
+            searchParams.delete('difficultyId')
+            return setSearchParams(searchParams)
+        }
+
+        setSearchParams((prev) => {
+            prev.set('difficultyId', difficulty)
+            return prev
+        })
+    }, [difficulty])
+
     return (
         <div>
             <h3 className="mb-2 font-semibold">Recipe Difficulty</h3>
             <div className="flex flex-wrap gap-2">
-                {['Easy', 'Medium', 'Hard'].map((level) => (
+                {['Easy', 'Medium', 'Hard'].map((level, index) => (
                     <button
+                        aria-selected={difficulty === String(index + 1)}
+                        aria-label={level}
                         key={level}
-                        className="rounded-full border border-gray-300 px-3 py-1 text-sm hover:bg-gray-100"
+                        className="rounded-full border border-gray-300 px-3 py-1 text-sm transition-colors hover:bg-gray-100 aria-[selected=true]:aria-[label=Easy]:bg-green-500 aria-[selected=true]:aria-[label=Hard]:bg-red-500 aria-[selected=true]:aria-[label=Medium]:bg-yellow-500 aria-[selected=true]:text-zinc-50"
+                        onClick={() =>
+                            handleDifficultyChange(String(index + 1))
+                        }
                     >
                         {level}
                     </button>
@@ -14,4 +42,4 @@ export default function RecipeDifficulty() {
             </div>
         </div>
     )
-} 
+}
