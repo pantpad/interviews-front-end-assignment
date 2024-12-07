9ce8fb0fd1d140fc74aeabfa366796e2ada6b71e
pantpad <138708449+pantpad@users.noreply.github.com>
2024-12-07 10:34:31 +0100

    feat: add placeholder when images are loading

--- frontend/src/components/Recipes/RecipeCard.tsx
+++ frontend/src/components/Recipes/RecipeCard.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router'
 import { Recipe } from '../../api/recipe'
 import { useRecipesContext } from '../../context/recipes-context'
+import { useState } from 'react'
 
 type RecipeCardProps = {
     recipe: Recipe
@@ -10,18 +11,30 @@ type RecipeCardProps = {
 export default function RecipeCard({ recipe, noLink }: RecipeCardProps) {
     const { name, image, cuisineId, dietId, difficultyId, id } = recipe
     const { cuisines, diets, difficulties } = useRecipesContext()
+    const [isLoading, setIsLoading] = useState(true)
+
+    const fullImageUrl = `http://localhost:8080${image}`
+
+    const loadingImage = isLoading ? (
+        <div className="absolute inset-0 aspect-square h-full w-full animate-pulse rounded-md bg-gray-200" />
+    ) : null
 
     return (
         <Link to={`/recipes/${id}`}>
             <article className="flex flex-wrap overflow-hidden rounded-lg bg-white p-2 shadow transition hover:shadow-md">
                 <figure className="relative h-full w-full sm:max-w-64">
-                    <img
-                        src={`http://localhost:8080${image}`}
-                        alt={name}
-                        height={64}
-                        width={64}
-                        className="aspect-square h-full w-full rounded-md object-cover"
-                    />
+                    <>
+                        {loadingImage}
+                        <img
+                            src={fullImageUrl}
+                            alt={name}
+                            height={64}
+                            width={64}
+                            className="aspect-square h-full w-full rounded-md object-cover aria-busy:opacity-0"
+                            onLoad={() => setIsLoading(false)}
+                            aria-busy={isLoading}
+                        />
+                    </>
                 </figure>
                 <div className="flex flex-1 flex-col justify-between px-2">
                     <section>
