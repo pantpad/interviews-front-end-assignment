64cab26007098f9e9b82b854af3c7463bd610313
Federico Di Marco <138708449+pantpad@users.noreply.github.com>
2024-12-09 16:07:41 +0100

    feat: add placeholder when images are loading (#9)
    * feat: add placeholder when images are loading
    
    * feat: remove options button, style on hover
    
    * refactor(recipeCard): image url is hidden using env var

--- frontend/src/components/Recipes/RecipeCard.tsx
+++ frontend/src/components/Recipes/RecipeCard.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router'
 import { Recipe } from '../../api/recipe'
 import { useRecipesContext } from '../../context/recipes-context'
+import { useState } from 'react'
 
 type RecipeCardProps = {
     recipe: Recipe
@@ -10,18 +11,33 @@ type RecipeCardProps = {
 export default function RecipeCard({ recipe, noLink }: RecipeCardProps) {
     const { name, image, cuisineId, dietId, difficultyId, id } = recipe
     const { cuisines, diets, difficulties } = useRecipesContext()
+    const [isLoading, setIsLoading] = useState(true)
+
+    const fullImageUrl = `${import.meta.env.VITE_API_ENDPOINT}${image}`
+
+    const loadingImage = isLoading ? (
+        <div className="absolute inset-0 aspect-square h-full w-full animate-pulse rounded-md bg-gray-200" />
+    ) : null
 
     return (
         <Link to={`/recipes/${id}`}>
-            <article className="flex flex-wrap overflow-hidden rounded-lg bg-white p-2 shadow transition hover:shadow-md">
+            <article
+                data-clickable={!noLink}
+                className="flex select-text flex-wrap overflow-hidden rounded-lg bg-white p-2 shadow transition hover:shadow-md data-[clickable=true]:hover:scale-[1.01] data-[clickable=true]:hover:opacity-80"
+            >
                 <figure className="relative h-full w-full sm:max-w-64">
-                    <img
-                        src={`http://localhost:8080${image}`}
-                        alt={name}
-                        height={64}
-                        width={64}
-                        className="aspect-square h-full w-full rounded-md object-cover"
-                    />
+                    <>
+                        {loadingImage}
+                        <img
+                            src={fullImageUrl}
+                            alt={name}
+                            height={64}
+                            width={64}
+                            className="aspect-square h-full w-full rounded-md object-cover aria-busy:opacity-0"
+                            onLoad={() => setIsLoading(false)}
+                            aria-busy={isLoading}
+                        />
+                    </>
                 </figure>
                 <div className="flex flex-1 flex-col justify-between px-2">
                     <section>
@@ -30,11 +46,6 @@ export default function RecipeCard({ recipe, noLink }: RecipeCardProps) {
                         <p>Diet: {diets[Number(dietId)]}</p>
                         <p>Difficulty: {difficulties[Number(difficultyId)]}</p>
                     </section>
-                    {!noLink ? (
-                        <button className="mt-4 w-full rounded-md bg-red-500 py-2 text-white">
-                            View Options
-                        </button>
-                    ) : null}
                 </div>
             </article>
         </Link>
