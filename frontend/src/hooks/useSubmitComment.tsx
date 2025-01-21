73d074a8118fb167e8d5950e145a32836445c5f0
pantpad <138708449+pantpad@users.noreply.github.com>
2025-01-21 18:23:18 +0100

    feat(recipeComments): add optimistic update on comment

--- frontend/src/hooks/useSubmitComment.tsx
+++ frontend/src/hooks/useSubmitComment.tsx
@@ -9,17 +9,54 @@ export function useSubmitComment() {
     return useMutation({
         mutationKey,
         mutationFn: submitComment,
-        onSuccess: (newComment, { recipeId }) => {
-            console.log('Comment added successfully')
+        onMutate: async ({ formData, recipeId }) => {
+            // cancel the query to avoid race conditions
+            await queryClient.cancelQueries({
+                queryKey: ['recipeComments', { recipeId }],
+            })
+
+            // get the snapshot that will be used to rollback the optimistic update
+            const snapshotBeforeOptimisticUpdate = queryClient.getQueryData([
+                'recipeComments',
+                { recipeId },
+            ])
+
+            // create comment object (like the one returned by the server)
+            const formObject = Object.fromEntries(formData.entries())
+            const newComment = {
+                ...formObject,
+                id: Date.now().toString(),
+                rating: Number(formObject.rating),
+                date: new Date(),
+            }
+
+            // actual optimistic update
             queryClient.setQueryData(
                 ['recipeComments', { recipeId }],
                 (recipeComments: RecipeCommentType[]) => {
-                    return [...recipeComments, newComment]
+                    return recipeComments
+                        ? [...recipeComments, newComment]
+                        : [newComment]
                 }
             )
+
+            // return a function to rollback the optimistic update
+            return () => {
+                queryClient.setQueryData(
+                    ['recipeComments', { recipeId }],
+                    snapshotBeforeOptimisticUpdate
+                )
+            }
         },
-        onError: (error) => {
+        onError: (error, _, rollback) => {
             console.log('Error adding comment', error)
+            alert('Error adding comment')
+            rollback?.()
+        },
+        onSettled: (_, __, { recipeId }) => {
+            return queryClient.invalidateQueries({
+                queryKey: ['recipeComments', { recipeId }],
+            })
         },
     })
 }
