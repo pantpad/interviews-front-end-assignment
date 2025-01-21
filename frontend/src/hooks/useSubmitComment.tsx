8172b0e477ffbc3b8e95d9b66455be0b342f5fba
pantpad <138708449+pantpad@users.noreply.github.com>
2025-01-21 17:05:22 +0100

    feat(recipeComment): set query cache without invalidating query so that comment can be seen instantly

--- frontend/src/hooks/useSubmitComment.tsx
+++ frontend/src/hooks/useSubmitComment.tsx
@@ -1,14 +1,22 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { submitComment } from '../api/recipe'
+import { RecipeCommentType, submitComment } from '../api/recipe'
+
+export const mutationKey = ['submitComment']
 
 export function useSubmitComment() {
     const queryClient = useQueryClient()
 
     return useMutation({
+        mutationKey,
         mutationFn: submitComment,
-        onSuccess: () => {
+        onSuccess: (newComment, { recipeId }) => {
             console.log('Comment added successfully')
-            queryClient.invalidateQueries({ queryKey: ['recipeComments'] })
+            queryClient.setQueryData(
+                ['recipeComments', { recipeId }],
+                (recipeComments: RecipeCommentType[]) => {
+                    return [...recipeComments, newComment]
+                }
+            )
         },
         onError: (error) => {
             console.log('Error adding comment', error)
